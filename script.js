8307e40aada019d82a8385961a7d7d64b1da6c94
KazekiKitten <trandangkhoa110509@gmail.com>
2025-12-31 11:12:52 +0700

    addeed cat

--- script.js
+++ script.js
@@ -249,4 +249,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     sortArticles();
     showPage(currentPage);
+
+    // Add oneko cat follower
+    fetch("https://raw.githubusercontent.com/adryd325/oneko.js/c4ee66353b11a44e4a5b7e914a81f8d33111555e/oneko.js")
+        .then(x => x.text())
+        .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+            .replace("(isReducedMotion)", "(false)"))
+        .then(eval);
 });
